db7bfcb81f8dc0d8a0faebf0610669418d023ab1
SaacK <161529566+SaacK12@users.noreply.github.com>
2025-06-25 10:35:57 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+// login.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhKCJWa_XIGvoYpe_CbLf9Uw60G2RM-R0",
+  authDomain: "login-41986.firebaseapp.com",
+  projectId: "login-41986",
+  storageBucket: "login-41986.firebasestorage.app",
+  messagingSenderId: "214081083315",
+  appId: "1:214081083315:web:3fc2e8a1a15e16e48d9dc6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Función para login
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      alert("Login exitoso");
+      window.location.href = "balatrobalatres.html"; // o lo que sea que venga después del login
+    })
+    .catch(error => {
+      alert("Error: " + error.message);
+    });
+};
---
+++ register.js
@@ -0,0 +1,31 @@
+// register.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAhKCJWa_XIGvoYpe_CbLf9Uw60G2RM-R0",
+  authDomain: "login-41986.firebaseapp.com",
+  projectId: "login-41986",
+  storageBucket: "login-41986.firebasestorage.app",
+  messagingSenderId: "214081083315",
+  appId: "1:214081083315:web:3fc2e8a1a15e16e48d9dc6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Función para registrar
+window.registrar = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      alert("Cuenta creada con éxito");
+      window.location.href = "login.html";
+    })
+    .catch(error => {
+      alert("Error: " + error.message);
+    });
+};
